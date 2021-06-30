de4354a1d31298a23b9568f3cbd22b3c03f35be6
Hardik <19it.hardikprajapati@adit.ac.in>
2021-06-30 11:38:27 +0530

    CSS added

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,43 @@
 <script>
     import Starter from './screens/Starter.svelte'
     import Quiz from './screens/Quiz.svelte'
-    import {isStart} from './store/Store.js'
+    import {
+        isStart
+    } from './store/Store.js'
 </script>
 
-<div>
-<h1>My Quiz</h1>
-    {#if $isStart}
-        <Quiz/>
-    {:else}
-        <Starter />
-    {/if}
-</div>
+
+    <div class='App'>
+        <div class="title">
+            <h1>My Quiz</h1>
+        </div>
+        {#if $isStart}
+            <Quiz/>
+        {:else}
+        <div class="starter">
+            <Starter />
+        </div>
+        {/if}
+    </div>
+
+
+<style>
+
+    .App{
+        background: rgb(255, 255, 255);
+        max-width: 700px;
+        margin: 0 auto;
+        min-height: 90vh;
+        margin-top: 5vh;
+        border-radius: var(--border-radius);
+        box-shadow: var(--shadow-2);
+    }
+    .title{
+        font-family: var(--bodyFont);
+        text-align: center;
+    }
+    .starter{
+        margin: 0 auto;
+        width: 400px;
+    }
+</style>
